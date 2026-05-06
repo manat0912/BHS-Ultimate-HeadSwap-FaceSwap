339286964b3d136e16c9c73c42b95def533c8272
6Morpheus6 <wanton-decay@gmx.net>
2026-05-06 22:48:26 +0200

    remove obsolete downloads

--- install.js
+++ install.js
@@ -86,20 +86,7 @@ module.exports = {
     {
       method: "hf.download",
       params: {
-        "path":"app",
-        "_": [ "olesheva/head_swap_qwen_edit" ]
-      }
-    },
-    {
-      method: "hf.download",
-      params: {
         "_": [ "tonera/FLUX.2-klein-4B-fp8-diffusers" ]
-      }
-    },
-    {
-      method: "hf.download",
-      params: {
-        "_": [ "rootlocalghost/FLUX.2-klein-9B-FP8" ]
       }
     },
     {
